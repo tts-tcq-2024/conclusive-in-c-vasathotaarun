189960b8796cef9ce56a9dc65d292d0b389b7b2f
vasathotaarun <vasathotaarun00@gmail.com>
2024-08-23 17:18:56 +0530

    Create breach_detection.cpp

---
+++ breach_detection.cpp
@@ -0,0 +1,11 @@
+#include "breach_detection.h"
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+    if (value < lowerLimit) {
+        return TOO_LOW;
+    }
+    if (value > upperLimit) {
+        return TOO_HIGH;
+    }
+    return NORMAL;
+}
