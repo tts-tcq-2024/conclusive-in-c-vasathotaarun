4051625b9be36b1380443e71872bbb2c12b78251
vasathotaarun <vasathotaarun00@gmail.com>
2024-08-23 17:20:40 +0530

    Create temperature_classification.h

---
+++ temperature_classification.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "breach_detection.h"
+
+typedef enum {
+    PASSIVE_COOLING,
+    HI_ACTIVE_COOLING,
+    MED_ACTIVE_COOLING
+} CoolingType;
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
