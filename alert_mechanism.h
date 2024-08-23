70c8d50f72e2b8a0cde4eef9d0d9d3e1a57feb7a
vasathotaarun <vasathotaarun00@gmail.com>
2024-08-23 17:18:21 +0530

    Create alert_mechanism.h

---
+++ alert_mechanism.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "breach_detection.h"
+
+typedef enum {
+    TO_CONTROLLER,
+    TO_EMAIL
+} AlertTarget;
+
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
